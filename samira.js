d89cdfb0eed9e6e42a1e00dd397dd5a1c587f5d8
Samira-yy <126532122+Samira-yy@users.noreply.github.com>
2025-12-18 20:32:55 +0400

    Update samira.js

--- samira.js
+++ samira.js
@@ -1,1 +1,32 @@
+// Farmer object class
+class Farmer {
+    constructor(id, name, waterUsage, farmSize, date) {
+        this.id = id;
+        this.name = name;
+        this.waterUsage = waterUsage;
+        this.farmSize = farmSize;
+        this.date = date;
+    }
+}
 
+// Company object class
+class Company {
+    constructor(id, name, regDate, email, totalFarms) {
+        this.id = id;
+        this.name = name;
+        this.registrationDate = regDate;
+        this.contactEmail = email;
+        this.totalFarms = totalFarms;
+    }
+}
+
+// Irrigation Record object class
+class IrrigationRecord {
+    constructor(id, company, amount, location, date) {
+        this.id = id;
+        this.companyName = company;
+        this.amount = amount;
+        this.location = location;
+        this.recordDate = date;
+    }
+}
